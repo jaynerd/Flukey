86a96079356063a58d278c8e949f67ebb6a1a1cd
unknown <j4yn3rd@gmail.com>
2018-06-17 03:42:13 +1200

    uuid identifier added

--- app/src/main/java/com/namyoon/flukey/InstantChatActivity.java
+++ app/src/main/java/com/namyoon/flukey/InstantChatActivity.java
@@ -1,13 +1,20 @@
 package com.namyoon.flukey;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class InstantChatActivity extends AppCompatActivity {
+    private String uuid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instant_chat);
+        InstantUser instantUser = new InstantUser();
+        Context context = getApplicationContext();
+        uuid = instantUser.getUuid(context);
+        Toast.makeText(context, uuid, Toast.LENGTH_LONG).show();
     }
 }
