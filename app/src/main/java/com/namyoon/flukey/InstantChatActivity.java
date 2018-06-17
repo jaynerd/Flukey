a988b68dc1b7e7dec7a42f2cbb290f4ee805a958
unknown <j4yn3rd@gmail.com>
2018-06-18 00:47:23 +1200

    database integration

--- app/src/main/java/com/namyoon/flukey/InstantChatActivity.java
+++ app/src/main/java/com/namyoon/flukey/InstantChatActivity.java
@@ -5,8 +5,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class InstantChatActivity extends AppCompatActivity {
     private String uuid;
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("InstantChat");
+    private Map<String, Object> map = new HashMap<String, Object>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,8 @@ public class InstantChatActivity extends AppCompatActivity {
         InstantUser instantUser = new InstantUser();
         Context context = getApplicationContext();
         uuid = instantUser.getUuid(context);
+        map.put(uuid, "");
+        reference.updateChildren(map);
         Toast.makeText(context, uuid, Toast.LENGTH_LONG).show();
     }
 }
